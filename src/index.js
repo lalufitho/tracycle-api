65c005151d8a52899cfafa50251c5e7d6ca16ca3
lalufitho <lalu.fitho8@gmail.com>
2023-06-14 00:12:40 +0800

    vercel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
 
-const router = express.Router();
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
-const serverless = require('serverless-http');
 
 const port = process.env.PORT || 3000;
 require('./config/db');
@@ -18,8 +16,6 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'pdf'));
 app.use('/', require('./routes'));
 
-// app.listen(port, () => {
-//   console.log(`Server running at PORT ${port}...`);
-// });
-app.use('/.netlify/functions/api', router);
-module.exports.handler = serverless(app);
+app.listen(port, () => {
+  console.log(`Server running at PORT ${port}...`);
+});
